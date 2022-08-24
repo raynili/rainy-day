ab3380b769d231f1df84433e4d2ff2ad9f01c14d
Rayni Li <raynili@Raynis-MacBook-Air.local>
2022-08-24 19:19:17 -0400

    created directory page

---
+++ src/components/directory/directory.jsx
@@ -0,0 +1,14 @@
+import './directory.scss';
+import CategoryItem from '../category-item/category-item';
+
+const Directory = ({categories}) => {
+    return (
+        <div className="directory-container">
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category}/>
+            ))}
+        </div>
+    )
+}
+
+export default Directory;

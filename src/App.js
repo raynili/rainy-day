1fa0fd3b41282c13132bcb87e7208ea39dc60ad4
Rayni Li <raynili@Raynis-MacBook-Air.local>
2022-08-29 21:16:08 -0400

    add sign in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home';
 import Navigation from './routes/navigation/navigation';
+import SignIn from './routes/sign-in/sign-in';
 
 const Shop = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop/>} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
